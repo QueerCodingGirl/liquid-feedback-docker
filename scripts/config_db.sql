135cf53a0c884a568a1254a3b0715a2bb2d07042
Markus Toran <markus@toranm.me>
2021-12-26 12:22:28 +0100

    Update config_db.sql

--- scripts/config_db.sql
+++ scripts/config_db.sql
@@ -4,22 +4,25 @@
 
 INSERT INTO system_setting (member_ttl) VALUES ('1 year');
 
--- INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (false, '1 hour', 20, 6);
--- INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (false, '1 day', 80, 12);
--- INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 hour', 200, 60);
--- INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 day', 800, 120);
+INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (false, '1 hour', 20, 6);
+INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (false, '1 day', 80, 12);
+INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 hour', 200, 60);
+INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 day', 800, 120);
 
 INSERT INTO "policy" (
         "index",
         "active",
         "name",
         "description",
-        "admission_time",
+        "min_admission_time",
+        "max_admission_time",
         "discussion_time",
         "verification_time",
         "voting_time",
+        "issue_quorum",
         "issue_quorum_num",
         "issue_quorum_den",
+        "initiative_quorum",
         "initiative_quorum_num",
         "initiative_quorum_den"
     ) VALUES (
@@ -27,34 +30,37 @@ INSERT INTO "policy" (
         TRUE,
         'Extensive proceeding',
         DEFAULT,
+        '15 days',
         '1 month',
         '5 months',
         '1 month',
         '3 weeks',
-        10, 100,
-        10, 100
+        1, 10, 100,
+        1, 10, 100
     ), (
         2,
         TRUE,
         'Standard proceeding',
         DEFAULT,
+        '15 days',
         '1 month',
         '1 month',
         '1 week',
         '1 week',
-        10, 100,
-        10, 100
+        1, 10, 100,
+        1, 10, 100
     ), (
        3,
        TRUE,
        'Fast proceeding',
        DEFAULT,
+       '20 hours',
        '48 hours',
        '3 hours',
        '1 hour',
        '20 hours',
-        1, 100,
-        1, 100 );
+        1, 1, 100,
+        1, 1, 100 );
     
 INSERT INTO "privilege" ("unit_id", "member_id", "voting_right")
   SELECT 1 AS "unit_id", "id" AS "member_id", TRUE AS "voting_right"
@@ -66,6 +72,5 @@ INSERT INTO area (unit_id, name) VALUES (1, 'Default area');
 
 INSERT INTO allowed_policy (area_id, policy_id, default_policy) VALUES (1, 1, TRUE);
 
--- Admin user with empty password
-INSERT INTO member (login, name, admin, password, activated, last_activity)
-VALUES ('admin', 'Administrator', TRUE, '$1$.EMPTY.$LDufa24OE2HZFXAXh71Eb1', NOW(), NOW());
+-- Admin user with invite sesam
+INSERT INTO member (invite_code, admin) VALUES ('sesam', true);
